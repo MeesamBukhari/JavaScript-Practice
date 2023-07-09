f749e1723d8098902d91c20ad59ae1060557ab79
Syed Meesam Turab <meesamturab@gmail.com>
2023-07-09 10:41:53 +0500

    JS Practice

---
+++ Variable/Variable Example/main.js
@@ -0,0 +1,16 @@
+var age = 25;
+var nationality = "Pakistani";
+var feePaid = true;
+
+alert(age);
+alert(nationality);
+alert(feePaid);
+
+var a;
+a = 67;
+console.log(a);
+
+var currentYear = 2023;
+console.log(currentYear);
+currentYear = 2024;
+console.log(currentYear);
---
+++ Variable/Variables For Strings/main.js
@@ -0,0 +1,7 @@
+var name1 = "Mark";
+console.log(name1);
+var name1 = "John";
+console.log(name1);
+
+var message = "It's alright";
+console.log(message);
