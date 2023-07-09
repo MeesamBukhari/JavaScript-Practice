a757afba4e046e41772da4301f0cb4639b918192
Syed Meesam Turab <meesamturab@gmail.com>
2023-07-09 10:41:20 +0500

    JS Practice

--- Variable/Variables For Booleans/main.js
+++
@@ -1,2 +0,0 @@
-var isFeePaid = true;
-alert(isFeePaid);
--- Variable/Variables For Strings/main.js
+++
@@ -1,7 +0,0 @@
-var name1 = "Mark";
-console.log(name1);
-var name1 = "John";
-console.log(name1);
-
-var message = "It's alright";
-console.log(message);
