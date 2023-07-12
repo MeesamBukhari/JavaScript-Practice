289a53d84fbe2b605a8657edbc805fbefb66bf0a
Syed Meesam Turab <meesamturab@gmail.com>
2023-07-12 08:43:20 +0500

    JS Practice

---
+++ Break & Continue/main.js
@@ -0,0 +1,37 @@
+// Break Statement
+console.log("Beginning Of Loop");
+for (var i=0; i<8; i++) {
+   if (i == 4) {
+      break;
+   }
+   console.log("I = " +i);
+}
+console.log("End Of Loop");
+
+console.log("Beginning Of Loop");
+for (var i=0; i<5; i++) {
+   if (i == 3) {
+      break;
+   }
+   console.log("I = " +i)
+}
+console.log("End Of Loop");
+
+// Continue Statement
+console.log("Beginning Of Loop");
+for (var i=0; i<8; i++) {
+   if (i == 4) {
+      continue
+   }
+   console.log("I = " +i);
+}
+console.log("End Of Loop");
+
+console.log("Beginning Of Loop");
+for (var i=0; i<15; i++) {
+   if (i == 11) {
+      continue;
+   }
+   console.log("I = " +i)
+}
+console.log("End Of Loop");
