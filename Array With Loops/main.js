e2cc10f9b08dc26a1e570c51e48678d5d3044de8
Syed Meesam Turab <meesamturab@gmail.com>
2023-07-17 11:41:44 +0500

    JS Practice

---
+++ Array With Loops/main.js
@@ -0,0 +1,25 @@
+var foods = ["Pizza", "Burger", "Snacks"];
+for (var i=0; i<foods.length; i++) {
+   console.log(foods[i]);
+}
+
+var num = [4, 78, 54, 5, 6];
+for (var i=0; i<num.length; i++) {
+   console.log(num[i]);
+}
+
+var arr = [4, 78, 54, 5, 6];
+for (var i=0; i<arr.length; i++) {
+   arr[i] = arr[i] +2;
+}
+for (var j=0; j<arr.length; j++) {
+   console.log(arr[j]);
+}
+
+var arr = [];
+for (var i=0; i<5; i++) {
+   arr[i] = i*4;
+}
+for (var j=0; j<arr.length; j++) {
+   console.log(arr[j]);
+}
