5f6b42c099b902571c3b30baddc26335322166e7
Syed Meesam Turab <meesamturab@gmail.com>
2023-07-18 20:40:11 +0500

    JS Practice

---
+++ Unshift Function/main.js
@@ -0,0 +1,12 @@
+var foods = [];
+foods.push("Pizza");
+foods.push("Burger");
+foods.push("Snacks");
+foods.push("Fries");
+console.log(foods);
+console.log("Length of Foods Array = "+foods.length)
+console.log(foods[0]);
+foods.unshift("Sandwich");
+console.log(foods[0]);
+console.log(foods[1]);
+console.log("Length of Foods Array = "+foods.length)
