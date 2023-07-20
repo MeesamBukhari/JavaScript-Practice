2664a8b2b2272dd563db18a478a9f81f576dd358
Syed Meesam Turab <meesamturab@gmail.com>
2023-07-20 21:25:38 +0500

    JS Practice

---
+++ Escape Characters/main.js
@@ -0,0 +1,10 @@
+var a = "Hello\'World";     // Single Quote
+console.log(a);
+var b = "Hello\"World";     // Double Quote
+console.log(b);
+var c = "Hello\\World";     // Backslash
+console.log(c);
+var d = "Hello\nWorld";     // New Line
+console.log(d);
+var e = "Hello\tWorld";     // Horizontal Tab
+console.log(e);
