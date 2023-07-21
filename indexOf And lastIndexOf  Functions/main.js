f137c649aa8d1b267f1990ce071a1c3f7cb2fefa
Syed Meesam Turab <meesamturab@gmail.com>
2023-07-21 21:05:59 +0500

    JS Practice

---
+++ indexOf And lastIndexOf  Functions/main.js
@@ -0,0 +1,55 @@
+var a = "To be or not to be";
+var b = a.indexOf("be");     // returns 3
+console.log(b);
+
+var a = "To be or not to be";
+var b = a.indexOf("be", 10);     // returns 16
+console.log(b);
+
+var a = "To be or not to be";
+var b = a.lastIndexOf("be");     // returns 16
+console.log(b);
+
+var a = "To be or not to be";
+var b = a.lastIndexOf("be", 10);     // returns 3
+console.log(b);
+
+var a = "To be or not to be";
+console.log("Length = "+a.length)
+var b = a.indexOf("b");
+console.log(b);
+
+var a = "To be or not to be";
+console.log("Length = "+a.length)
+var b = a.indexOf("v");
+console.log(b);
+
+var a = "To be or not to be";
+console.log("Length = "+a.length)
+var b = a.indexOf("be");
+console.log(b);
+
+var a = "To be or not to be";
+console.log("Length = "+a.length)
+var b = a.indexOf("be or");
+console.log(b);
+
+var a = "To be or not to be";
+console.log("Length = "+a.length)
+var b = a.indexOf("beor");
+console.log(b);
+
+var a = "To be or not to be";
+console.log("Length = "+a.length)
+var b = a.indexOf("be", 10);
+console.log(b);
+
+var a = "To be or not to be";
+console.log("Length = "+a.length)
+var b = a.lastIndexOf("be");
+console.log(b);
+
+var a = "To be or not to be";
+console.log("Length = "+a.length)
+var b = a.lastIndexOf("be", 10);
+console.log(b);
