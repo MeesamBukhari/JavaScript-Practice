ed935581d0d1ceeb9fbf25d89ecc389f07162446
Syed Meesam Turab <meesamturab@gmail.com>
2023-07-24 19:23:17 -0900

    JS Practice

--- toFixed Function/main.js
+++ toFixed Function/main.js
@@ -3,3 +3,7 @@ var num2 = Number(prompt("Enter second number of three numbers to find average a
 var num3 = Number(prompt("Enter third and the last number to find average among all:"));
 var average = (num1 + num2 + num3) / 3;
 alert ("The average of three numbers is " + Number(average.toFixed(3)));
+
+var average = (15 + 23 + 39) / 3;
+var avg = average.toFixed(3);
+console.log(avg);
