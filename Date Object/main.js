803217df3a5a9891a7e1a6b9b739e143ae33d1ef
Syed Meesam Turab <meesamturab@gmail.com>
2023-07-26 09:18:35 +0500

    JS Practice

--- Date Object/main.js
+++ Date Object/main.js
@@ -1,2 +1,17 @@
 var today = new Date();
 console.log(today);
+
+var today = new Date("2050 7 3");
+console.log(today);
+
+var today = new Date("2050/7/3 10:15:20");
+console.log(today);
+
+var today = Date.now();    // Time in Milliseconds
+console.log(today);
+
+var today = new Date(1690259974358);
+console.log(today);
+
+var today = new Date("August 7 2050 10:15:20");
+console.log(today);
