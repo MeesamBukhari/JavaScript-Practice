d6524a5be8124a19bfa96742608bdf7bf0fb0e7e
Syed Meesam Turab <meesamturab@gmail.com>
2023-07-30 00:17:46 +0500

    JS Practice

---
+++ Function As Expression/main.js
@@ -0,0 +1,11 @@
+var sum = function (a, b) {      // Function As Expression
+   return a + b;
+};
+var c = sum (4,5);
+console.log(c);
+
+var square = function (num) {
+   return num * num;
+};
+var f = square (4);
+console.log(f);
