dae7db8648ca7b716d80202a2ccb66584064a3ea
MeesamBukhari <meesamturab@gmail.com>
2023-08-18 15:59:10 +0500

    JS Practice

---
+++ Switch Statement/main.js
@@ -0,0 +1,48 @@
+var day = prompt("Enter the number of day of week: ");
+switch (day) {
+   case 0:
+      alert("Today is Sunday");
+      break;
+      case '0':
+         alert("Today is Sunday");
+         break;
+      case '1':
+         alert("Today is Monday");
+         break;
+      case '2':
+         alert("Today is Tuesday");
+         break;
+      case '3':
+         alert("Today is Wednesday");
+         break;
+      case '4':
+         alert("Today is Thursday");
+         break;
+      case '5':
+         alert("Today is Friday");
+         break;
+      case '6':
+         alert("Today is Saturday");
+         break;
+      default:
+         alert("Enter valid number of day of week");
+}
+
+
+var letter = prompt ("Enter a letter: ");
+switch (letter) {
+   case 'a':
+   case 'e':
+   case 'i':
+   case 'o':
+   case 'u':
+   case 'A':
+   case 'E':
+   case 'I':
+   case 'O':
+   case 'U':
+      alert("Vowel Entered!");
+      break;
+   default:
+      alert("Consonant Entered!");
+}
