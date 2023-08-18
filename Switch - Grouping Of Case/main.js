92165b619f47639f6a696c0af54c66b9330e05bb
MeesamBukhari <meesamturab@gmail.com>
2023-08-18 15:59:45 +0500

    JS Practice

---
+++ Switch - Grouping Of Case/main.js
@@ -0,0 +1,9 @@
+var day = prompt ("Enter the number of day of week: ");
+switch (day) {
+   case 6:
+   case 0:
+      console.log("Yaaaa! It's Weekend.");
+      break;
+   default:
+      console.log("Looking forward to the Weekend");
+}
